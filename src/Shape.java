b8533b27daa95df94c2cfb5a1bffa92e4c57bedf
sonnguyenece <61034270+sonnguyenece@users.noreply.github.com>
2020-03-27 13:58:28 +0700

    TriangleClass
    em gửi bài tập thiết kế lớp Triangle ạ

--- src/Shape.java
+++ src/Shape.java
@@ -1,5 +1,41 @@
-public  abstract class Shape {
+public class Shape {
+    private String color="red";
+    private boolean filled = false;
 
-    abstract void draw(int a);//khai bao
+    public Shape() {
 
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+//    public boolean getFilled() {
+//        return filled;
+//    }
+
+    @Override
+    public String toString() {
+        return "Color: "
+                + color
+                + ", Filled: "
+                + (isFilled() ? "yes" : "not filled");
+    }
 }
