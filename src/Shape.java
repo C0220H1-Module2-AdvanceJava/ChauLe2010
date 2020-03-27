12ad30ea7a4e390ccdb8294d1262ac4c75a025f0
linh-phamdang21 <61008000+linh-phamdang21@users.noreply.github.com>
2020-03-27 13:51:01 +0700

    Em gửi chị Code để review ạ

--- src/Shape.java
+++ src/Shape.java
@@ -1,5 +1,37 @@
-public  abstract class Shape {
+public class Shape {
+    private String color = "green";
+    private boolean filled = false;
 
-    abstract void draw(int a);//khai bao
+    public Shape() {
 
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", " + (this.filled? "filled" : "unfilled") +
+                '}';
+    }
 }
