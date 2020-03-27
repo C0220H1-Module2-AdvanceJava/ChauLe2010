cc69341cede0423a1754e8eff30d001de8424e49
huyfr <61008601+huyfr@users.noreply.github.com>
2020-03-27 14:08:57 +0700

    Em gửi chị bài tập kế thừa
    Bài tập thiết kế và triển khai lớp Triangle

--- src/Shape.java
+++ src/Shape.java
@@ -1,5 +1,33 @@
-public  abstract class Shape {
+public class Shape {
+    private String color = "green";
+    private boolean filled = true;
 
-    abstract void draw(int a);//khai bao
+    public Shape() {
+    }
 
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Color: " + getColor() + ", " + (isFilled() ? "filled, " : "not filled, ");
+    }
 }
