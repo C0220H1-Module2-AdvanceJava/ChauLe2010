517f8547a9f0f36f7a238c24e91d8691c6500e15
Lê Bá Đạt <61006100+lebadat1996@users.noreply.github.com>
2020-03-27 15:12:17 +0700

    interface 'Resizeable'

--- src/Square.java
+++ src/Square.java
@@ -1,12 +1,45 @@
-public class Square  implements IShape,IColor{
-    int side;
-    @Override
+public class Square extends Shape {
+    private double size;
+
+    public Square() {
+
+    }
+
+    public Square(double size) {
+
+    }
+
+    public Square(double size, String color, boolean filled) {
+        super(color, filled);
+        this.size = size;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
     public double getArea() {
-        return side*side;
+        return this.size * this.size;
+    }
+
+    public double getPerimeter() {
+        return this.size * 4;
     }
 
     @Override
-    public double getPerimeter() {
-        return 4*side;
+    public String toString() {
+        return "\nA Square size = " + getSize() +
+                "\nArea = " + getArea() +
+                "\nPerimeter = " + getPerimeter() +
+                super.toString();
+    }
+
+    @Override
+    public void resize(double percent) {
+        this.size *= percent / 200;
     }
 }
