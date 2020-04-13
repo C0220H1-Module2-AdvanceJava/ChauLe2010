286970dd5d3d34bf51a832c7e8269fee4de2a398
huyfr <61008601+huyfr@users.noreply.github.com>
2020-04-13 09:22:26 +0700

    Bài tập quản lý sản phẩm
    Em gửi chị Châu bài quản lý sản phẩm lưu ra file nhị phân

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,18 @@
-import java.util.Scanner;
-import java.util.function.DoubleToIntFunction;
-
 public class Main {
     public static void main(String[] args) {
-        double a, b, c;
-        String color;
-        Scanner sc = new Scanner(System.in);
-        try {
-            do {
-                System.out.print("Enter side a: ");
-                a = sc.nextDouble();
-                System.out.print("Enter side b: ");
-                b = sc.nextDouble();
-                System.out.print("Enter side c: ");
-                c = sc.nextDouble();
-                if (a + b < c || b + c < a || c + a < b) {
-                    System.out.println("Not triangle");
-                }
-            } while (a + b < c || b + c < a || c + a < b);
+        FunctionProduct functionProduct = new FunctionProduct();
 
-            System.out.print("Enter a color: ");
-            color = sc.next();
+        functionProduct.creatObject();
 
-            Triangle triangle = new Triangle(color, true, a, b, c);
-            System.out.println(triangle.toString());
+        String path = "G:\\codegym\\module2\\btQuanLySanPham\\src\\example.txt";
+        functionProduct.writeToFile(functionProduct.containsObject, path);
 
-            Shape shape = new Shape();
-            System.out.println(shape.toString());
+        functionProduct.readFromFile(path);
 
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        functionProduct.displayProduct();
 
+        Product searchById = functionProduct.searchProduct(3);
+
+        System.out.println(searchById.toString());
     }
 }
